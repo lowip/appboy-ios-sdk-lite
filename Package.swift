dcac3d059cdb72c2c38f7f05b1b8a36d908a6ef0
Louis Bur <louisbur92@gmail.com>
2020-10-30 14:47:12 -0400

    WIP

---
+++ Package.swift
@@ -0,0 +1,62 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "Appboy_iOS_SDK",
+  defaultLocalization: "en",
+  platforms: [
+    .iOS(.v9)
+  ],
+  products: [
+    .library(name: "AppboyKit", type: .static, targets: ["AppboyKit"]),
+    .library(name: "AppboyUI", targets: ["AppboyUI"]),
+    .library(name: "AppboyPushStory", type: .static, targets: ["AppboyPushStory"])
+  ],
+  dependencies: [
+    .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2")
+  ],
+  targets: [
+    .binaryTarget(
+      name: "AppboyKitLibrary",
+      url: "https://github.com/Appboy/appboy-ios-sdk/releases/download/3.29.1/AppboyKitLibrary.xcframework.zip",
+      checksum: "92d644be71dd5c51373e3a9d4f0c493bdf2f55af4c3444ff8142fae30447a72b"
+    ),
+    .target(
+      name: "AppboyKit",
+      dependencies: ["SDWebImage", "AppboyKitLibrary"],
+      path: "AppboyKit",
+      resources: [
+        .process("Appboy.bundle"),
+        .process("headers/AppboyKitLibrary/ZipArchive_LICENSE.txt")
+      ],
+      publicHeadersPath: "headers/AppboyKitLibrary",
+      linkerSettings: [
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("QuartzCore"),
+        .linkedFramework("CoreImage"),
+        .linkedFramework("CoreText"),
+        .linkedFramework("WebKit"),
+        .linkedFramework("UserNotifications"),
+        .linkedFramework("StoreKit"),
+        .linkedFramework("CoreTelephony", .when(platforms: [.iOS])),
+        .linkedLibrary("z"),
+      ]
+    ),
+    .target(
+      name: "AppboyUI",
+      dependencies: ["AppboyKit"],
+      path: "AppboyUI",
+      resources: [
+        .process("ABKNewsFeed/Resources"),
+        .process("ABKInAppMessage/Resources"),
+        .process("ABKContentCards/Resources")
+      ],
+      publicHeadersPath: "include/AppboyUI"
+    ),
+    .binaryTarget(
+      name: "AppboyPushStory",
+      path: "Appboy-Push-Story/AppboyPushStory.xcframework"
+    )
+  ]
+)
