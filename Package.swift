308ed184fc3a6ec41b8335eaea65d1127bf8a5b0
Louis Bur <louisbur92@gmail.com>
2021-03-09 15:11:51 -0500

    WIP

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     .binaryTarget(
       name: "AppboyKitLibrary",
       url: "https://conscious-selection.surge.sh/AppboyKitLibrary.xcframework.zip",
-      checksum: "ab197785fcd148682657aaade5b36e545dca013d17a8ec0287c704127ac65051"
+      checksum: "958021ab550256974bec5c7d8174e5953e3a1e986ff6acadf96f17bfe2e1a053"
     ),
     .target(
       name: "AppboyKit",
@@ -30,7 +30,6 @@ let package = Package(
         .process("Appboy.bundle"),
         .process("headers/AppboyKitLibrary/ZipArchive_LICENSE.txt")
       ],
-      publicHeadersPath: "headers/AppboyKitLibrary",
       linkerSettings: [
         .linkedFramework("SystemConfiguration"),
         .linkedFramework("QuartzCore"),
@@ -57,7 +56,7 @@ let package = Package(
     .binaryTarget(
       name: "AppboyPushStoryFramework",
       url: "https://conscious-selection.surge.sh/AppboyPushStoryFramework.xcframework.zip",
-      checksum: "3b0073d9505ded2e366e9ad37cf8eccd62962f5edf6591e5ea78efed1640e8d9"
+      checksum: "3b65f489eda8cc0e54fa37c8eca9dc2ddc8c6e00f76559fca1a282963faba73a"
     ),
     .target(
       name: "AppboyPushStory",
