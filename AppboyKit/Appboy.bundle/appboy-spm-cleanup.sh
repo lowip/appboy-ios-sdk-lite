d0cf0a153a567ffaed86b703f9a9dbe606c2161d
Louis Bur <louisbur92@gmail.com>
2021-03-10 15:08:41 -0500

    WIP

--- AppboyKit/Appboy.bundle/appboy-spm-cleanup.sh
+++ AppboyKit/Appboy.bundle/appboy-spm-cleanup.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # AppboyKitLibrary
-find "$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME" -name libAppboyKitLibrary.a -follow -exec rm {} \;
+find "${TARGET_BUILD_DIR}/$FULL_PRODUCT_NAME" -name libAppboyKitLibrary.a -follow -exec rm {} \;
 
 # AppboyPushStory
-find "$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME" -name "AppboyPushStory.framework" -follow -exec rm -r {} \;
+find "${TARGET_BUILD_DIR}/$FULL_PRODUCT_NAME" -name "AppboyPushStory.framework" -follow -exec rm -r {} \;
 
